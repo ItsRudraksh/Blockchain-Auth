58e7cf295e4e62b4dfc4a49aecd3cde4610dc523
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2025-03-15 00:17:45 +0530

    refactor: remove Besu local network configuration from Hardhat setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,13 +14,6 @@ module.exports = {
     },
   },
   networks: {
-    besulocal: {
-      chainId: 1337,
-      url: "http://127.0.0.1:8545", // Besu local RPC (if that's where Besu is running)
-      accounts: [
-        `0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3`,
-      ],
-    },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 11155111,
